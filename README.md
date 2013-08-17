c2c24b76af2b8648e224864edc5f1d3b9721e2ef
Jonathan Kolyer <devcrew9000@gmail.com>
2013-08-16 17:44:13 -0700

    added DEVMODE and various fixes

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ railsweet
 =========
 
 Bash scripts creating suite of rails apps built on common database engine.  Creates 4 rails apps (web app, api, admin, data services) and an engine shared by all.  
+
+Usage
+
+Assumptions
+* Biased towards postgres DB
+* Will not generate DB instance
+
--- railsweet.bash
+++ railsweet.bash
@@ -8,46 +8,70 @@ export APP_API=api
 export APP_WEB=web
 export APP_SRV=services
 export APP_ADM=admin
+export APP_BIN=bin
 export SWEET_DB=postgresql
 
 export RAILSWEET_BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export RAILSWEET_BASE_DIR=$RAILSWEET_BIN_DIR/..
 
+export DEVMODE=0
+
 function _init_railsapps {
-     rails plugin new $APP --mountable --database=$SWEET_DB
-     rails new $APP_API --database=$SWEET_DB
-     rails new $APP_WEB --database=$SWEET_DB
-     rails new $APP_SRV --database=$SWEET_DB
-     rails new $APP_ADM --database=$SWEET_DB
+    echo "*** creating rails plugin"
+    rails plugin new $APP --mountable --database=$SWEET_DB
+    sed -i -e "s/TODO: Your name/$USER/g" $APP_DIR/$APP/$APP.gemspec
+    sed -i -e "s/TODO: Your email/$USER@example.com/g" $APP_DIR/$APP/$APP.gemspec
+    sed -i -e "s/TODO: //g" $APP_DIR/$APP/$APP.gemspec
+    sed -i -e "s/TODO/http:\/\/example.com/g" $APP_DIR/$APP/$APP.gemspec
+    # FIXME:  ensure this function is defined, warned if not
+    eval $APP'_init_default_gems'
+
+    echo "*** creating $APP_API"
+    rails new $APP_API --database=$SWEET_DB
+
+     if [[ "x$DEVMODE" != "x1" ]]; then
+	 echo "*** creating $APP_WEB"
+	 rails new $APP_WEB --database=$SWEET_DB
+	 echo "*** creating $APP_SRV"
+	 rails new $APP_SRV --database=$SWEET_DB
+	 echo "*** creating $APP_ADM"
+	 rails new $APP_ADM --database=$SWEET_DB
+     fi
 }
 
 function _init_databaseyml {
-     pushd $APP_API/config
-     mv database.yml ../../$APP/config
-     ln -s ../../$APP/config/database.yml .
-     cd ../../$APP_WEB/config
-     rm database.yml
-     ln -s ../../$APP/config/database.yml .
-     cd ../../$APP_SRV/config
-     rm database.yml
-     ln -s ../../$APP/config/database.yml .
-     cd ../../$APP_ADM/config
-     rm database.yml
-     ln -s ../../$APP/config/database.yml .
+    pushd $APP_API/config
+    mv database.yml ../../$APP/config
+    ln -s ../../$APP/config/database.yml .
+    if [[ "x$DEVMODE" != "x1" ]]; then
+	cd ../../$APP_WEB/config
+	rm database.yml
+	ln -s ../../$APP/config/database.yml .
+	cd ../../$APP_SRV/config
+	rm database.yml
+	ln -s ../../$APP/config/database.yml .
+	cd ../../$APP_ADM/config
+	rm database.yml
+	ln -s ../../$APP/config/database.yml .
+     fi
      popd
 }
 
 function _init_bundle_install {
     echo "** $APP_DIR/$APP; bundle install"
     cd  $APP_DIR/$APP; bundle install
+
     echo "** $APP_DIR/$APP_API; bundle install"
     cd  $APP_DIR/$APP_API; bundle install
-    echo "** $APP_DIR/$APP_WEB; bundle install"
-    cd  $APP_DIR/$APP_WEB; bundle install
-    echo "** $APP_DIR/$APP_SRV; bundle install"
-    cd  $APP_DIR/$APP_SRV; bundle install
-    echo "** $APP_DIR/$APP_ADM; bundle install"
-    cd  $APP_DIR/$APP_ADM; bundle install
+
+    if [[ "x$DEVMODE" != "x1" ]]; then
+	echo "** $APP_DIR/$APP_WEB; bundle install"
+	cd  $APP_DIR/$APP_WEB; bundle install
+	echo "** $APP_DIR/$APP_SRV; bundle install"
+	cd  $APP_DIR/$APP_SRV; bundle install
+	echo "** $APP_DIR/$APP_ADM; bundle install"
+	cd  $APP_DIR/$APP_ADM; bundle install
+    fi
     
     # echo "$APP_DIR; rails generate devise:install"
     # cd $APP_DIR/$APP; rails generate devise:install
@@ -55,36 +79,58 @@ function _init_bundle_install {
 
 function _init_gemfiles {
     echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_API/Gemfile
-    echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_WEB/Gemfile
-    echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_SRV/Gemfile
-    echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_ADM/Gemfile
-    $APP_init_default_gems $APP
+    if [[ "x$DEVMODE" != "x1" ]]; then
+	echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_WEB/Gemfile
+	echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_SRV/Gemfile
+	echo "gem '$APP', path: '../$APP'" >> $APP_DIR/$APP_ADM/Gemfile
+
+	echo "gem 'activeadmin', github: 'gregbell/active_admin', branch: 'rails4'" >> $APP_DIR/$APP_ADM/Gemfile
+	
+	eval $APP'_did_init_gemfiles'  # FIXME:  check if function exists
+    fi
+    
 }
 
 function _init_routes {
     export NG_NAME=`echo "$(echo "$APP" | sed 's/.*/\u&/')"`
     sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_API/config/routes.rb
-    sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_WEB/config/routes.rb
-    sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_SRV/config/routes.rb
-    sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_ADM/config/routes.rb
+
+    if [[ "x$DEVMODE" != "x1" ]]; then
+	sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_WEB/config/routes.rb
+	sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_SRV/config/routes.rb
+	sed -i -e "2 i mount "$NG_NAME"::Engine, at: '/'" $APP_DIR/$APP_ADM/config/routes.rb
+
+	sed -i -e "3 i devise_for :admin_users, ActiveAdmin::Devise.config" $APP_DIR/$APP_ADM/config/routes.rb
+	sed -i -e "4 i ActiveAdmin.routes(self)" $APP_DIR/$APP_ADM/config/routes.rb
+    fi
 }
 
 function app_sweet_setup {
+    # FIXME:  check for APP argument, check for definition of $APP_init_default_gems
+    # FIXME:  add dry-run option
+    # FIXME:  reimplement with thor
+
     export APP=$1
-    export APP_DIR=$dcbin/../$APP
-    source $dcbin/$APP.bash
+    export APP_DIR=`pwd`/$APP
+    source `pwd`/bin/$APP.bash  # default value, check for function arg
 
     mkdir $APP_DIR
     pushd $APP_DIR
+
     echo "*** init rails apps"
-    _init_railsapps $APP
+    _init_railsapps
+
     echo "*** init database yml"
-    _init_databaseyml $APP
+    _init_databaseyml
+
     echo "*** init routes"
-    _init_routes $APP
+    _init_routes
+
     echo "*** init gemfiles"
-    _init_gemfiles $APP
+    _init_gemfiles
+
     echo "*** init bundle install"
-    _init_bundle_install $APP
+    _init_bundle_install
+
     popd
 }
